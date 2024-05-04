1eca416ae32343e9980d744aa6124f8018cdf828
Hyperfied <hyperfied@outlook.com>
2024-05-04 21:32:52 +0100

    Split into panels, make docking workey

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,78 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            textBox1 = new TextBox();
             treeView1 = new TreeView();
             splitter1 = new Splitter();
+            panel2 = new Panel();
             listView1 = new ListView();
             columnHeader1 = new ColumnHeader();
-            textBox1 = new TextBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(treeView1);
+            panel1.Controls.Add(textBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 450);
+            panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Dock = DockStyle.Top;
+            textBox1.Location = new Point(0, 0);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(200, 23);
+            textBox1.TabIndex = 1;
+            textBox1.KeyPress += textBox1_KeyPress;
             // 
             // treeView1
             // 
-            treeView1.Dock = DockStyle.Left;
-            treeView1.Location = new Point(0, 0);
+            treeView1.Dock = DockStyle.Fill;
+            treeView1.Location = new Point(0, 23);
             treeView1.Name = "treeView1";
-            treeView1.Size = new Size(300, 450);
+            treeView1.Size = new Size(200, 427);
             treeView1.TabIndex = 0;
             treeView1.BeforeExpand += treeView1_BeforeExpand;
             treeView1.AfterSelect += treeView1_AfterSelect;
             // 
             // splitter1
             // 
-            splitter1.Location = new Point(300, 0);
+            splitter1.Location = new Point(200, 0);
             splitter1.Name = "splitter1";
             splitter1.Size = new Size(3, 450);
             splitter1.TabIndex = 1;
             splitter1.TabStop = false;
             // 
+            // panel2
+            // 
+            panel2.Controls.Add(listView1);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(203, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(597, 450);
+            panel2.TabIndex = 2;
+            // 
             // listView1
             // 
             listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1 });
             listView1.Dock = DockStyle.Fill;
-            listView1.Location = new Point(303, 0);
+            listView1.Location = new Point(0, 0);
             listView1.Name = "listView1";
-            listView1.Size = new Size(497, 450);
-            listView1.TabIndex = 2;
+            listView1.Size = new Size(597, 450);
+            listView1.TabIndex = 0;
             listView1.UseCompatibleStateImageBehavior = false;
             listView1.View = View.Details;
             // 
             // columnHeader1
             // 
-            columnHeader1.Text = "Name";
-            columnHeader1.Width = 200;
-            // 
-            // textBox1
-            // 
-            textBox1.Dock = DockStyle.Top;
-            textBox1.Location = new Point(303, 0);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(497, 23);
-            textBox1.TabIndex = 3;
-            textBox1.KeyPress += textBox1_KeyPress;
+            columnHeader1.Text = "File";
+            columnHeader1.Width = 150;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(textBox1);
-            Controls.Add(listView1);
+            Controls.Add(panel2);
             Controls.Add(splitter1);
-            Controls.Add(treeView1);
+            Controls.Add(panel1);
             Name = "Form1";
             Text = "File Browser";
             Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
+        private Panel panel1;
+        private TextBox textBox1;
         private TreeView treeView1;
         private Splitter splitter1;
+        private Panel panel2;
         private ListView listView1;
         private ColumnHeader columnHeader1;
-        private TextBox textBox1;
     }
 }
